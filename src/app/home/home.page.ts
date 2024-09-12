a844fc4dfcce98b79cc6d996a765dbad76338920
GearlessVins <vi.valenzuelat@duocuc.cl>
2024-09-11 21:01:19 -0300

    .

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Importa Router para la redirección
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,30 @@ export class HomePage implements OnInit {
   isClassStarted: boolean = false;
   usuario: string | null = null;
 
-  constructor() {}
+  // Definición de los botones del alert
+  public alertButtons = [
+    {
+      text: 'No',
+      cssClass: 'alert-button-cancel',
+    },
+    {
+      text: 'Yes',
+      cssClass: 'alert-button-confirm',
+      handler: () => {
+        // Lógica para redirigir al login
+        this.redirectToLogin();
+      },
+    },
+  ];
+
+  constructor(private router: Router) {} // Inyecta Router
 
   ngOnInit() {
     this.usuario = localStorage.getItem('usuario');
+  }
+
+  // Método para redirigir al login
+  redirectToLogin() {
+    this.router.navigate(['/login']);
   }
 }
